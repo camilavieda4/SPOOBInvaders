37de3d10b853aeaf08651df284a118009d2b47e4
camilavieda4 <45082424+camilavieda4@users.noreply.github.com>
2018-11-18 20:34:22 -0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,10 @@
+package Presentacion;
+
+public class Main{
+	public static SpaceInvaderGUI sp;
+	public static void main(String[] args) {
+		sp = new SpaceInvaderGUI();
+		sp.setVisible(true);
+	
+}
+}
